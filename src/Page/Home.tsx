d3aecb16b4397976a9021d477dcd16da9375c657
75ways <75ways@75wayss-Mac-mini.local>
2024-01-31 17:53:51 +0530

    some changes

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import './Home.css'
 
 const Home = () => {
   return (
-    <>
+    <div className='homeContainer'>
     <h2>Welcome to Quiz</h2>
     <p>Please click below for starting your quiz</p>
     <Link to='/quiz'><button>Start Quiz</button></Link>
+    </div>
 
-    </>
+  
   )
 }
 
